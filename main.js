02be3fc745c0c69cc66507f161735cd39d41ac4e
boomafromboomiland <lubomira.kovacova@avon.com>
2022-02-15 19:48:59 +0100

    úkol 1

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // tady je místo pro náš program
 
-let nadpis = document.querySelector ('h1');
+/*let nadpis = document.querySelector ('h1');
 nadpis.classList.add('aktivni');
 
 // nadpis.classList.remove('aktivni');
@@ -14,3 +14,26 @@ function priNajetiNaStvorec() {
   pocitadlo++;
 };
 
+function stvorec() {
+  ctverecekZeleny.classList.toggle('ctverecek zluty')
+}
+
+*/
+
+function stucni () {
+  let odstavec = document.querySelector('p');
+  odstavec.style.fontWeight = 'bold';
+}
+
+function zmenFarbu() {
+  let odstavec = document.querySelector('p');
+  odstavec.classList.toggle('red');
+}
+
+let velkostPisma = 16;
+
+function zvacsi() {
+  let odstavec = document.querySelector('p');
+  console.log('velkostPisma' + 1);
+  velkostPisma++;
+}
