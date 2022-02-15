2cad706b36bd3e95c57d1e89ea54de7fa741a520
boomafromboomiland <lubomira.kovacova@avon.com>
2022-02-15 18:50:57 +0100

    Add, remove, toggle

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 // tady je místo pro náš program
+
+let nadpis = document.querySelector ('h1');
+nadpis.classList.add('aktivni');
+
+// nadpis.classList.remove('aktivni');
+// nadpis.classList.toggle('aktivni'); (zapne, ak nie je; vypne, ak je --> roll na menu)
+
+
+
